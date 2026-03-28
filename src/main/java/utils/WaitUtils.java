d6ea413f3aea168704d211690834188c9dabdece
archutce <archutce@gmail.com>
2026-03-28 12:20:13 +0530

    increased wait timings

--- src/main/java/utils/WaitUtils.java
+++ src/main/java/utils/WaitUtils.java
@@ -15,7 +15,7 @@ public class WaitUtils {
 	
 	public WaitUtils(WebDriver driver) {
 		this.driver=driver;
-		wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait=new WebDriverWait(driver, Duration.ofSeconds(15));
 		
 	}
 
